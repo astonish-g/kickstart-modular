8ea50fc0d588b27d80d1bc55142e8607445969d8
Astonish <gatmacagit@gmail.com>
2024-08-11 13:27:59 +0300

    added autpairs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,6 +35,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
+  require 'kickstart/plugins/autopairs',
+
   -- require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/catppuccin',
